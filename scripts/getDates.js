7c43e709514f376170fc6032a74c8ce188957aa4
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-09-17 20:21:43 -0300

    Add copyright symbol to dynamically populate it

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,10 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const currentYear = new Date().getFullYear();
     // Update the copyright year in the footer's first paragraph
     const copyrightYearElement = document.querySelector('#copyright-year');
-    copyrightYearElement.textContent = currentYear;
+    copyrightYearElement.textContent = "©"+currentYear;
+
+
     // Get the last modified date of the document
     const lastModifiedDate = new Date(document.lastModified);
-
     // Format the date as a string (e.g., "September 17, 2023")
     const formattedLastModifiedDate = lastModifiedDate.toLocaleDateString('en-US', {
         year: 'numeric',
