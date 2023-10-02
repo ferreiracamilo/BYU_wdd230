463a126b7b28de28fa440fc074ac9de64120b599
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-10-01 23:43:30 -0300

    Add comment to js

--- chamber/scripts/darkMode.js
+++ chamber/scripts/darkMode.js
@@ -1,11 +1,11 @@
 const darkModeToggle = document.getElementById('darkModeToggle');
 const body = document.body;
-const navElement = document.getElementById("navbar-container"); // Get all <nav> elements (returns a collection)
+const navElement = document.getElementById("navbar-container");
 
 darkModeToggle.addEventListener('change', () => {
     if (darkModeToggle.checked) {
         body.classList.add('dark-mode-body');
-        navElement.classList.add('dark-mode-nav');
+        navElement.classList.add('dark-mode-nav'); //nav bar will still be relevant against black background with a border line activated by this class
     } else {
         body.classList.remove('dark-mode-body');
         navElement.classList.remove('dark-mode-nav');
