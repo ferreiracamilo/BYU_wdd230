10eb2dcecdf6c17961020010843d66903b60d9f2
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-10-01 23:42:38 -0300

    push changes for dark toggle mode

--- chamber/scripts/darkMode.js
+++ chamber/scripts/darkMode.js
@@ -1,10 +1,13 @@
 const darkModeToggle = document.getElementById('darkModeToggle');
 const body = document.body;
+const navElement = document.getElementById("navbar-container"); // Get all <nav> elements (returns a collection)
 
 darkModeToggle.addEventListener('change', () => {
     if (darkModeToggle.checked) {
-        body.classList.add('dark-mode');
+        body.classList.add('dark-mode-body');
+        navElement.classList.add('dark-mode-nav');
     } else {
-        body.classList.remove('dark-mode');
+        body.classList.remove('dark-mode-body');
+        navElement.classList.remove('dark-mode-nav');
     }
 });
