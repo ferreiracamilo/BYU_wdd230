42a316b2ecc4534e03be69cdac4b89ecd6465088
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-10-04 15:44:03 -0300

    Redesign newsletter to trigger only for new guess or visit 1 day or more

--- chamber/scripts/newsletter.js
+++ chamber/scripts/newsletter.js
@@ -2,9 +2,11 @@ const popupOverlay = document.getElementById('popup-overlay');
 const closePopupButton = document.getElementById('close-popup');
 
 // Show the pop-up when the page loads
+/*
 window.addEventListener('load', () => {
     popupOverlay.style.display = 'block';
 });
+*/
 
 closePopupButton.addEventListener('click', () => {
     popupOverlay.style.display = 'none';
@@ -17,3 +19,19 @@ newsletterForm.addEventListener('submit', (e) => {
     alert('Thank you for subscribing!');
     popupOverlay.style.display = 'none';
 });
+
+// Check if this is the user's first visit to set the variable for HOME PAGE CALL TO ACTION
+if (!localStorage.getItem('lastNewsletter')) {
+    localStorage.setItem('lastNewsletter', new Date().getTime());
+    popupOverlay.style.display = 'block';
+}else {
+    //There's a var called lastVisit within local storage so days diff will be checked
+    const lastNewsletterTimestamp = parseInt(localStorage.getItem('lastNewsletter'));
+    const currentTime = new Date().getTime();
+    const timeDiffMillis = currentTime - lastNewsletterTimestamp;
+    const timeDiffDays = Math.floor(timeDiffMillis / (1000 * 60 * 60 * 24));
+
+    if (timeDiffDays >= 1) {
+        popupOverlay.style.display = 'block';
+    }
+}
