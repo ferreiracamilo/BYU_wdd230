69abe1858a84b04a1310fed472509ae8a1342405
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-10-08 12:51:16 -0300

    push update

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -4,7 +4,8 @@ const cards = document.querySelector('#cards');
 async function getProphetData(){
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.prophets); // temporary testing of data retreival
+    //console.table(data.prophets); // temporary testing of data retreival
+    displayProphets(data.prophets);
 }
 
 getProphetData();
