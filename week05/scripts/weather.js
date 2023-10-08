495cb4c7518870e23895cad3c47fc5a11bd681c0
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-10-08 19:41:01 -0300

    replace javascript

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -1,5 +1,3 @@
-// 49.75, 6.64
-
 const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
@@ -23,7 +21,7 @@ async function apiFetch(){
 }
 
 function displayResults(data){
-    currentTemp.innerHTML = "${data.main.temp}&deg;F";
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
     const iconsrc = "https://openweathermap.org/img/wn/"+ data.weather[0].icon  + ".png";
     let desc = data.weather[0].description;
     weatherIcon.setAttribute("src", iconsrc);
