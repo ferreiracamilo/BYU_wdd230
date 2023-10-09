460c2132a2e07ff4332ed4b7b45854bacefeb98e
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-10-09 01:32:36 -0300

    update style to hide data explanation in list view

--- chamber/scripts/loadCompanies.js
+++ chamber/scripts/loadCompanies.js
@@ -39,26 +39,51 @@ const displayMembers = (members) => {
         const card = document.createElement("section");
         const icon = document.createElement("img");
         const name = document.createElement("h3");
-        const membership = document.createElement("p");
-        const website = document.createElement("p");
-        const address = document.createElement("p");
-        const opening = document.createElement("p");
 
         // Build the image icon by setting all the relevant attributes
         icon.setAttribute('src', member.icon);
         icon.setAttribute('alt', "Icon of " + member.name);
         icon.setAttribute('loading', 'lazy');
 
-        // Set teh value of full name h2
+        //Set the value of h3
         name.textContent = member.name;
 
-        membership.textContent = "Membership: " + member.membership;
+        //Define spans first which then will be dissappear in fake list/table view by css
+        const membership = document.createElement("p");
+        const membership_span = document.createElement("span");
+        membership_span.setAttribute("class","remove-span-list");
+        membership_span.textContent = "Membership: ";
+        const membership_span_text = document.createElement("span");
+        membership_span_text.textContent = member.membership;
+        membership.append(membership_span);
+        membership.append(membership_span_text);
 
-        website.textContent = "Website: " + member.website;
+        const website = document.createElement("p");
+        const website_span = document.createElement("span");
+        website_span.setAttribute("class","remove-span-list");
+        website_span.textContent = "Website: ";
+        const website_span_text = document.createElement("span");
+        website_span_text.textContent = member.website;
+        website.append(website_span);
+        website.append(website_span_text);
 
-        address.textContent = "Address: " + member.address;
+        const address = document.createElement("p");
+        const address_span = document.createElement("span");
+        address_span.setAttribute("class","remove-span-list");
+        address_span.textContent = "Address: ";
+        const address_span_text = document.createElement("span");
+        address_span_text.textContent = "Address: " + member.address;
+        address.append(address_span);
+        address.append(address_span_text);
 
-        opening.textContent = "Opening date: " + member.opening;
+        const opening = document.createElement("p");
+        const opening_span = document.createElement("span");
+        opening_span.setAttribute("class","remove-span-list");
+        opening_span.textContent = "Opening date: ";
+        const opening_span_text = document.createElement("span");
+        opening_span_text.textContent = member.opening;
+        opening.append(opening_span);
+        opening.append(opening_span_text)
 
         // Append the section(card) with the created elements
         card.appendChild(icon);
