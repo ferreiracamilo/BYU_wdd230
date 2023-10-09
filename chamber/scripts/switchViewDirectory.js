43d94b4cb575ee4bad3bf9494567006bbb9f278a
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-10-09 00:24:34 -0300

    partial update

---
+++ chamber/scripts/switchViewDirectory.js
@@ -0,0 +1,18 @@
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
