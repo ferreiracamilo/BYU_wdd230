21e73b3c8f3f0a11b1805eb8fb9cce271ac30aed
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-10-08 21:40:56 -0300

    add id for list

--- scripts/links.js
+++ scripts/links.js
@@ -5,6 +5,7 @@ async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     console.log(data);
+    //displayLinks(data)
   }
   
   getLinks();
